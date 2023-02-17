f7e1e5f3afd74b5ef16b7e97aeecc570600adace
nalinioliver <noliver@smith.edu>
2023-02-16 23:07:48 -0500

    Finished assignment 3

--- Conversation.java
+++ Conversation.java
@@ -1,6 +1,72 @@
-class Conversation {
+import java.util.Scanner;
+import java.util.Random;
+import java.util.ArrayList;
 
+/**
+ * Creates a welcome message for the user, and then prompts a user input for the number of conversation rounds. Afterwards, the user undergoes a conversation for the number of rounds before the transcript of the conversation is printed.
+ * @author Nalini Oliver
+ * Resources: Jordan Crouser's office hours (Monday 2/13), CSC 120 TA Hours (Monday 2/13 and Thursday 2/16), W3 schools "Java ArrayList"
+ */
+class Conversation {
   public static void main(String[] arguments) {
-    // You will start the conversation here.
-  }
-}
+    /** Creates and names a new scanner (called "in") that will prompt a user input */
+    Scanner in; 
+    in = new Scanner(System.in); 
+    /** Creates an array called "transcript" that can store strings */
+    ArrayList<String> transcript = new ArrayList<String>();
+    /** Adds the word "TRANSCRIPT" to the array list */
+    transcript.add("TRANSCRIPT:");
+    /** Prints out the message "How many rounds?" */
+    System.out.println("How many rounds?: ");
+    /** Adds the message "How many rounds?" to the array list */
+    transcript.add("How many rounds?: ");
+    /** Will cause the program to view the inital input of the user to be an integer (necessary for the "for" loop) */
+    Integer userInput = in.nextInt();
+    /** Will change the user input (interpretted as an integer) to a String (necessary in order to add this to the array list) */
+    String newuserInput = Integer.toString(userInput);
+    /** Will add the user input (the number of rounds) to the array list */
+    transcript.add(newuserInput);
+
+    /** Creates and names a new scanner (called "forloop") that will allow for a new user input */
+    Scanner forloop; 
+    forloop = new Scanner(System.in);
+
+    /** Prints out the message "Hi there! What's on your mind?" */
+    System.out.println("Hi there! What's on your mind?: ");
+    /** Adds the string "Hi there! What's on your mind?" to the array list */
+    transcript.add("Hi there! What's on your mind?: ");
+    /** Creates the variable "callandresponse" with the Random method */
+    Random callandresponse = new Random();
+    /** Creates an array of type string that act as the "canned responses" for the computer conversation */
+    String[] canned= {"What do you mean by that?", "Okay", "Interesting", "Mhm", "Tell me more"};
+    /** 
+     * Creates a "for" loop that starts at 0, and continues as long as the value of "i" is less than the user input (which represents the number of "rounds" of conversation). This loop will continue with the i++ 
+     * @param: userInput, which is the user input (integer) that specifies the number of rounds of conversation the user wants 
+     * @param: i++, will cause the "for" loop to continue to the next iteration of the conversation (prompting user input and computer response), until value of "i" (starting at 0), is equal to the value of userInput
+     */
+    for (int i = 0; i < userInput; i++) {
+    /** Takes the user input in the "for" loop and makes the program view it as a string */
+          String forloopInput = forloop.nextLine();
+    /** Adds user input in the "for" loop to the array known as "transcript" */
+          transcript.add(forloopInput);
+    /** Saves the index "length" (i.e- highest index number) by taking the length of the array and subtracting 1 (since it starts at 0). This is saved under "randomindex" variable, which will allow for a random selection from the canned responses array list (based on the callandresponse object saved with the "Random" method)  */
+          Integer randomindex = callandresponse.nextInt(canned.length - 1);
+    /** Prints out one of the strings in the canned responses array at random */
+          System.out.println(canned[randomindex]);
+    /** Adds the randomly generated canned response to the transcript */
+          transcript.add(canned[randomindex]); 
+        }
+    /** Prints out the message "Goodbye" */
+      System.out.println("Goodbye");
+    /** Saves the message "Goodbye" to the transcript array */
+      transcript.add("Goodbye");
+    /** Prints out the transcript array */
+      System.out.println(transcript);
+
+    /** Closes both scanners */
+      in.close();
+      forloop.close();  
+        }
+      } 
+  
+
--- reflection.md
+++ reflection.md
@@ -1,7 +1,20 @@
 Use this file to record your reflection on this assignment.
 
 - Which classmates did you consult with while working on this assignment?
+
+I consulted with Dakota on this assignment. 
+
 - Which session(s) of TA / office hours did you attend?
+
+I attended Jordan's Monday office hours (2/13). I also attended CSC TA hours on Monday (2/13) and Thursday (2/16). 
+
 - What are your initial impressions of Java? 
+Overall, I feel like I can't appreciate Java on it's own, because I'm constantly comparing it to Python. This is because other students I've known have said that Python is a much more intuitive and easy to use programming language, which already made Java feel daunting to me as I entered this course. I would agree that the terminology (methods, classes, etc.) as well as needing to specify everything (public, void, etc.) is overwhelming. However, I find that talking about it during CSC TA hours did make it feel more manegable to some degree/ not as scary as I intally thought!
+
 - Can you draw any conclusion about programming in general from the similarities or the differences between the two languages? 
+
+Overall, I would say that programming relies upon the need to categorize things into certain "types" and then attempts to provide tools that can manipulate and use objects of a certain "type" to create something new. In python, this idea is less prominent (as there isn't always a need to specify), but in both Python and Java, we see that there are tools specific to a certain "kind" of varaible that relies on this concept. In java, it is much more prominent, where (for exapmple) it only allows arrays to be formed of specific types. 
+
 - What worked, what didn't, what advice would you give someone taking this course in the future?
+
+For this assignment, I felt that I did a better job of knowing how much time I needed to get specific parts done. Even though I was unable to complete the part of the assignment where we attempt to mirror the words (while I had partial code it would make my program unable to run, so I ended up omitting it) I still felt that I made a lot of progress in using Java overall. This includes understanding the differences in syntax, building a for loop, and knowing the order for executing specific functions. However, this was still a huge challenge in other respects (evident in the fact that I still need to work on completeing the "mirroring" portion of this assignment). I feel that being able to translate the information we learn from Prof. Crouser's lectures to the assignment goals have been difficult for me, and something I would like to work on further.
